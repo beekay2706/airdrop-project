b27b8c367345b040992ab15f3fe3bad3d8e7a323
beekay2706 <barathas12@gmail.com>
2022-05-07 22:24:03 +0530

    fix errors and update getbalance function

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
 const {
     Connection,
     PublicKey,
-    ClusterApiUrl,
+    clusterApiUrl,
     Keypair,
     LAMPORTS_PER_SOL
 } = require("@solana/web3.js")
 
 const wallet = new Keypair()
 
-const publicKey =new PublicKey(wallet._keypair.publicKey)
+const publicKey = new PublicKey(wallet._keypair.publicKey)
 const secretKey = wallet._keypair.secretKey
 
-console.log(publicKey)
-console.log(secretKey)
+const getWalletBalance = async() => {
+    try {
+        const connection = new Connection(clusterApiUrl('devnet'), 'confirmed')
+        const walletBalance = await connection.getBalance(publicKey)
+        console.log(`Wallet balance is ${walletBalance}`)
+    } catch(err) {
+        console.error(err)
+    }
+}
+const main = async() => {
+    getWalletBalance()
+}
+main()
