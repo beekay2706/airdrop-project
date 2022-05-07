ef118594e7a922b5c42e144473efb2258387238c
beekay2706 <barathas12@gmail.com>
2022-05-07 21:36:28 +0530

    setup public and private keys

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ const {
     ClusterApiUrl,
     Keypair,
     LAMPORTS_PER_SOL
-} = require("@solana/web3.js");
+} = require("@solana/web3.js")
 
-const wallet = new Keypair();
+const wallet = new Keypair()
+
+const publicKey =new PublicKey(wallet._keypair.publicKey)
+const secretKey = wallet._keypair.secretKey
+
+console.log(publicKey)
+console.log(secretKey)
